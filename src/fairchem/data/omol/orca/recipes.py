7c3042758ad0daee3cfabc5bd56e921568d4cedf
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2025-08-19 12:30:07 -0500

    omdata cleanup (#1437)

--- src/fairchem/data/omol/orca/recipes.py
+++ src/fairchem/data/omol/orca/recipes.py
@@ -1,20 +1,17 @@
-"""
-Copyright (c) Meta Platforms, Inc. and affiliates.
-
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
-"""
 from __future__ import annotations
 
 import os
 
 import psutil
 from omdata.orca.calc import (
+    NBO_FLAGS,
     OPT_PARAMETERS,
     ORCA_BASIS,
     ORCA_BLOCKS,
     ORCA_FUNCTIONAL,
     ORCA_SIMPLE_INPUT,
+    Vertical,
+    get_symm_break_block,
 )
 from quacc.recipes.orca.core import run_and_summarize, run_and_summarize_opt
 
@@ -29,6 +26,9 @@ def single_point_calculation(
     orcablocks=None,
     nprocs=12,
     outputdir=os.getcwd(),
+    vertical=Vertical.Default,
+    nbo=False,
+    copy_files=None,
     **calc_kwargs,
 ):
     """
@@ -55,6 +55,8 @@ def single_point_calculation(
         List of `orcablocks` swaps for the calculator
     nprocs: int
         Number of processes to parallelize across
+    nbo: bool
+        Run NBO as part of the Orca calculation
     outputdir: str
         Directory to move results to upon completion
     calc_kwargs:
@@ -68,12 +70,19 @@ def single_point_calculation(
         orcasimpleinput = ORCA_SIMPLE_INPUT.copy()
     if orcablocks is None:
         orcablocks = ORCA_BLOCKS.copy()
+    if vertical == Vertical.MetalOrganics and spin_multiplicity == 1:
+        orcasimpleinput.append("UKS")
+        orcablocks.append(get_symm_break_block(atoms, charge))
+    if not nbo:
+        orcasimpleinput.extend(["NONBO", "NONPA"])
+    else:
+        orcablocks.append(NBO_FLAGS)
 
     nprocs = psutil.cpu_count(logical=False) if nprocs == "max" else nprocs
-    default_inputs = [xc, basis, "engrad", "normalprint"]
+    default_inputs = [xc, basis, "engrad"]
     default_blocks = [f"%pal nprocs {nprocs} end"]
 
-    return run_and_summarize(
+    doc = run_and_summarize(
         atoms,
         charge=charge,
         spin_multiplicity=spin_multiplicity,
@@ -81,8 +90,11 @@ def single_point_calculation(
         default_blocks=default_blocks,
         input_swaps=orcasimpleinput,
         block_swaps=orcablocks,
+        copy_files=copy_files,
         **calc_kwargs,
     )
+
+    return doc
 
 
 def ase_relaxation(
@@ -96,6 +108,10 @@ def ase_relaxation(
     nprocs=12,
     opt_params=None,
     outputdir=os.getcwd(),
+    vertical=Vertical.Default,
+    copy_files=None,
+    nbo=False,
+    step_counter_start=0,
     **calc_kwargs,
 ):
     """
@@ -124,6 +140,10 @@ def ase_relaxation(
         Number of processes to parallelize across
     opt_params: dict
         Dictionary of optimizer parameters
+    nbo: bool
+        Run NBO as part of the Orca calculation
+    step_counter_start: int
+        Index to start step counter from (used for optimization restarts)
     outputdir: str
         Directory to move results to upon completion
     calc_kwargs:
@@ -139,12 +159,19 @@ def ase_relaxation(
         orcablocks = ORCA_BLOCKS.copy()
     if opt_params is None:
         opt_params = OPT_PARAMETERS.copy()
+    if vertical == Vertical.MetalOrganics and spin_multiplicity == 1:
+        orcasimpleinput.append("UKS")
+        orcablocks.append(get_symm_break_block(atoms, charge))
+    if not nbo:
+        orcasimpleinput.extend(["NONBO", "NONPA"])
+    else:
+        orcablocks.append(NBO_FLAGS)
 
     nprocs = psutil.cpu_count(logical=False) if nprocs == "max" else nprocs
-    default_inputs = [xc, basis, "engrad", "normalprint"]
+    default_inputs = [xc, basis, "engrad"]
     default_blocks = [f"%pal nprocs {nprocs} end"]
 
-    return run_and_summarize_opt(
+    doc = run_and_summarize_opt(
         atoms,
         charge=charge,
         spin_multiplicity=spin_multiplicity,
@@ -153,5 +180,9 @@ def ase_relaxation(
         input_swaps=orcasimpleinput,
         block_swaps=orcablocks,
         opt_params=opt_params,
+        copy_files=copy_files,
+        step_counter_start=step_counter_start,
         **calc_kwargs,
     )
+
+    return doc
